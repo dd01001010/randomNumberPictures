a81d7eedaab61500c93621d1d0cd849bbebc4166
d01001010 <d01001010@hotmail.com>
2019-03-08 14:23:30 -0800

    working, not displaying correctly though.

---
+++ Interface.h
@@ -0,0 +1,25 @@
+#ifndef Interface_Header
+#define Interface_Header
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+const char MAX(10);
+
+class RNG{
+    private:
+        struct Data1
+	{char f_name[MAX];
+		char l_name[MAX];};
+    int node_Counter;
+
+    public:
+    Data1 Value[MAX];
+    void dMenu();
+    int create_Rng();
+    void great();
+    void cLs();
+};
+#endif
--- main.cpp
+++ main.cpp
@@ -1,33 +1,62 @@
-#include <iostream>
-#include <cstdlib>
-using namespace std;
 
-//#include "Interface.h"
+#include "Interface.h"
 
-int main(){cout << " Hello Jesus, Thank you! " << endl;
+int main() {
+	
+	RNG obj;
+	char usr_choice = 'Q' || 'q';
 
-    char usrName = 0;
-    char clicker = 0;
-    do
-    {
-        //great the user, ask for name
-        cout << " Hello what is your name ? " << endl;
-        cin >> usrName;
+	//beginning of Do-While statement 
+	do {
+		obj.great();
 
-        //use input to create new greating
-        cout << " Hello " << usrName << " nice to meet you " << endl;
-        //use input to start rng
-        //reap and do until user is done
-        clicker++;
-    } while (clicker < 2);
-    
+		cout << "Do you want to continue ?\n " << endl; cout << "Enter (Y)es or (Q)uit : ";
+		cin >> usr_choice;
+		cin.ignore(); //add in class
+	} // close do
+	while ((usr_choice == 'Y') || (usr_choice == 'y'));
+	return 0;
+}// end main
 
+void RNG::dMenu(){char choice = 0;
+	
+    cout << "(C)reate Random Number." << endl;
+	cout << "(Q)uit program by hitting the 'Q' key." << endl;
+	cin >> choice;
+	cin.ignore();
 
+	switch (choice) { // switch statement controls user input
+	case 'c':
+	case 'C':
+            great(); //great user
+ 			dMenu();// returns to top Display Menu
+		 break;
+	default:
+		break;
+	}//END SWITCH
+}//END TOP MENU
 
-
-    return 0;
+void RNG::great(){cLs(); char nPut[MAX];
+            int i = 0;
+            cout << " Hello, what is your name " << endl;
+            cin.getline(Value[i].f_name, MAX);
+            cout << " Thank you " << Value[i].f_name << ", we will start create Random numbers. " << endl;
+            system("pause");
+            i++;
+            create_Rng();
+                        //cin.ignore(); //add in class
 }
 
+int RNG::create_Rng(){cLs(); //system ("clear");
+     cout << " Access rng function correctly " << endl;
+}
 
-
-
+void RNG::cLs()
+{
+#ifdef WINDOWS
+    std::system("cls");
+#else
+    // Assume POSIX
+    std::system ("clear");
+#endif
+}
