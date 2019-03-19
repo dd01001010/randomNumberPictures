a8fd5bbf4abe77a456ca889ef366249dcad9dac0
d01001010 <d01001010@hotmail.com>
2019-03-18 19:24:13 -0700

    cut00

--- Interface.h
+++ Interface.h
@@ -1,3 +1,27 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2019 David Johnson 
+
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+
+/* code */
+
 #ifndef Interface_Header
 #define Interface_Header
 
@@ -5,24 +29,23 @@
 #include <cstdlib>
 #include <string>
 #include <ctime>
-#include <string>
 using namespace std;
 
-const char MAX(10);
+const char MAX(10); //or MAX = 10;
 
 class RNG{
     private:
         struct Data1
-	{char f_name[MAX];
+	{char f_name[MAX]; //character array
 		char l_name[MAX];};
-    int node_Counter;
+    
+    int node_Counter;// counter if needed in the future..  
 
     public:
-    Data1 Value[MAX]; //array of structures 
-    void dMenu();
-    void create_Rng();
-    void great();
-    void cLs();
-    
+    Data1 Value[MAX]; //array of structures, saves session information for future features if needed..  
+    void dMenu(); //display menu 
+    void create_Rng(); //create rndom numbers
+    void great(); //great user
+    void cLs(); //custom clear screen
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -1,55 +1,90 @@
 #include "Interface.h"
+// The MIT License (MIT)
 
-int main() {
-	
+// Copyright (c) 2019 David Johnson 
+
+//  Permission is hereby granted, free of charge, to any person obtaining a
+//  copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation
+//  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+//  DEALINGS IN THE SOFTWARE.
+
+/* code */
+
+int main()
+{
 	RNG obj;
 	char usr_choice = 'Q' || 'q';
-    obj.cLs();
+	obj.cLs();
 
-	//beginning of Do-While statement 
-	do {
+	//beginning of Do-While Loop
+	do
+	{
 		obj.great();
 
-		cout << "Do you want to continue ?\n " << endl; cout << "Enter (Y)es or (Q)uit : ";
+		cout << "Do you want to continue ?\n " << endl;
+		cout << "Enter (Y)es or (Q)uit : ";
 		cin >> usr_choice;
-		cin.ignore(); //add in class
-	} // close do
+		cin.ignore();									//add in class
+	}													// close do
 	while ((usr_choice == 'Y') || (usr_choice == 'y')); //check condition
-	return 0;}// end main
+	return 0;
+} // end main
 
-void RNG::dMenu(){char choice = 0;
-	
-    cout << "(C)reate Random Number." << endl;
+void RNG::dMenu()
+{
+	char choice = 0;
+
+	cout << "(C)reate Random Number." << endl;
 	cout << "(Q)uit program by hitting the 'Q' key." << endl;
 	cin >> choice;
 	cin.ignore();
 
-	switch (choice) { // switch statement controls user input
+	switch (choice)
+	{ // switch statement controls user input
 	case 'c':
 	case 'C':
-            great(); //great user
- 			dMenu();// returns to top Display Menu
-		 break;
+		great(); //great user
+		dMenu(); // returns to top Display Menu
+		break;
 	default:
 		break;
-	}//END SWITCH
-}//END TOP MENU
+	} //END SWITCH
+} //END TOP MENU
 
-void RNG::great(){cLs(); char nPut[MAX];
-            int i = 0;
-            cout << " Hello, what is your name ";
-            cin.getline(Value[i].f_name, MAX);
-            cout << " " << endl;
-            cout << " Thank you " << Value[i].f_name << ", we will start creating Random numbers now, " 
-                                                    "Press Enter to continue "<< endl;
-            cin.get(); 
-            //system("pause");
-            i++;
-            create_Rng();
-            cin.get();            //cin.ignore(); //add in class
+void RNG::great()
+{
+	cLs();
+	char nPut[MAX];
+	int i = 0;
+	cout << " Hello, enter your name to continue...  ";
+	cin.getline(Value[i].f_name, MAX);
+	cLs();
+	cout << " " << endl;
+	cout << " Thank you " << Value[i].f_name << ", we will start creating Random numbers.. "
+												"Press Enter to continue "
+		 << endl;
+	cin.get();
+
+	i++;
+	create_Rng();
+	cin.get();
 }
 
-void RNG::create_Rng(){cLs();
+void RNG::create_Rng()
+{cLs();
 	srand(time(0));
 	int rNg = (rand() % MAX) + 1;
 
@@ -59,58 +94,53 @@ void RNG::create_Rng(){cLs();
 	{ // switch statement controls user input
 	case 1:
 		system("gnome-terminal -x sh -c 'firefox https://previews.123rf.com/images/martialred/martialred1601/martialred160100213/51673234-number-1-one-fan-hand-glove-with-finger-raised-flat-vector-icon.jpg'");
-		 
 		break;
+
 	case 2:
 		system("gnome-terminal -x sh -c 'firefox http://pngimg.com/uploads/number2/Number%202%20PNG%20images%20free%20download_PNG14925.png'");
-
 		break;
+
 	case 3:
 		system("gnome-terminal -x sh -c 'firefox https://www.johnbrownhardware.co.uk/ekmps/shops/513a47/images/classic-designs-3-black-number-3-digit-pack-5-54617-p.jpg'");
-
 		break;
+
 	case 4:
 		system("gnome-terminal -x sh -c 'firefox https://vignette.wikia.nocookie.net/phobia/images/d/d0/4.jpg/revision/latest?cb=20161127143001'");
-		//great user
 		break;
+
 	case 5:
 		system("gnome-terminal -x sh -c 'firefox https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwg5LAww4T6MmRkQUwG3Cnsc6qTyOfCFkQ46tPvXv0gm5D90V1'");
-
 		break;
+
 	case 6:
 		system("gnome-terminal -x sh -c 'firefox https://blognumbers.files.wordpress.com/2010/09/6.jpg");
-
 		break;
 
 	case 7:
 		system("gnome-terminal -x sh -c 'firefox https://images-na.ssl-images-amazon.com/images/I/31muN9eG3yL._SX355_.jpg'");
-
-		great(); //great user
-				 //dMenu();// returns to top Display Menu
 		break;
+
 	case 8:
 		system("gnome-terminal -x sh -c 'firefox https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Huit.svg/1024px-Huit.svg.png'");
-
 		break;
+
 	case 9:
 		system("gnome-terminal -x sh -c 'firefox https://s12085.pcdn.co/wp-content/uploads/2013/10/B-9.jpg'");
 		cout << " 9 " << endl;
 		break;
+
 	default:
 		system("gnome-terminal -x sh -c 'firefox https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwAocoBM-ddlESN75Yj7Ed8Sqf1f81HfR3EhrNDDoyzeZw79kU'");
-
 		break;
 	}
 }
 
-//system()
-
 void RNG::cLs()
 {
 #ifdef WINDOWS
-    std::system("cls");
+	std::system("cls");
 #else
-    // Assume POSIX
-    std::system ("clear");
+	// Assume POSIX
+	std::system("clear");
 #endif
 }
