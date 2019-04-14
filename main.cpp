3b602a7daf0e648ed80cd6f6d5d4ff3c92ef9875
d01001010 <d01001010@hotmail.com>
2019-04-13 17:24:05 -0700

    using local files for displaying image.

--- main.cpp
+++ main.cpp
@@ -29,7 +29,12 @@ int main()
 	char usr_choice = 'Q' || 'q';
 	obj.cLs();
 
-	//
+	/*string numOne = "https://previews.123rf.com/images/martialred/martialred1601/martialred160100213/51673234-number-1-one-fan-hand-glove-with-finger-raised-flat-vector-icon.jpg'";
+
+	cout << " test 1" << endl;
+
+	system("gnome-terminal -x sh -c \'firefox" + numOne);
+	cin.get(); */
 
 	//beginning of Do-While Loop
 	do
@@ -69,6 +74,8 @@ void RNG::dMenu()
 void RNG::great()
 {
 	cLs();
+	//cout << system("cd num1_10_file; pwd") << endl;
+
 	int i = 0;
 	cout << " Hello, enter your name to continue...  ";
 	cin.getline(Value[i].f_name, MAX);
@@ -108,45 +115,54 @@ void RNG::rngCheck(int rng)
 	switch (rng)
 	{ // switch statement controls user input
 	case 1:
-		system("gnome-terminal -x sh -c 'firefox https://previews.123rf.com/images/martialred/martialred1601/martialred160100213/51673234-number-1-one-fan-hand-glove-with-finger-raised-flat-vector-icon.jpg'");
+		//system("gnome-terminal -x sh -c \'firefox https://previews.123rf.com/images/martialred/martialred1601/martialred160100213/51673234-number-1-one-fan-hand-glove-with-finger-raised-flat-vector-icon.jpg'");
+		system("cd num1_10_file; xdg-open one.jpg");
 		//system(" ");
 		break;
 
 	case 2:
-		system("gnome-terminal -x sh -c 'firefox http://pngimg.com/uploads/number2/Number%202%20PNG%20images%20free%20download_PNG14925.png'");
+		//system("gnome-terminal -x sh -c 'firefox http://pngimg.com/uploads/number2/Number%202%20PNG%20images%20free%20download_PNG14925.png'");
+		system("cd num1_10_file; xdg-open two.png");
 		break;
 
 	case 3:
-		system("gnome-terminal -x sh -c 'firefox https://www.johnbrownhardware.co.uk/ekmps/shops/513a47/images/classic-designs-3-black-number-3-digit-pack-5-54617-p.jpg'");
+		//system("gnome-terminal -x sh -c 'firefox https://www.johnbrownhardware.co.uk/ekmps/shops/513a47/images/classic-designs-3-black-number-3-digit-pack-5-54617-p.jpg'");
+		system("cd num1_10_file; xdg-open three.png");
 		break;
 
 	case 4:
-		system("gnome-terminal -x sh -c 'firefox https://vignette.wikia.nocookie.net/phobia/images/d/d0/4.jpg/revision/latest?cb=20161127143001'");
+		//system("gnome-terminal -x sh -c 'firefox https://vignette.wikia.nocookie.net/phobia/images/d/d0/4.jpg/revision/latest?cb=20161127143001'");
+		system("cd num1_10_file; xdg-open four.jpg");
 		break;
 
 	case 5:
-		system("gnome-terminal -x sh -c 'firefox https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwg5LAww4T6MmRkQUwG3Cnsc6qTyOfCFkQ46tPvXv0gm5D90V1'");
+		//system("gnome-terminal -x sh -c 'firefox https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwg5LAww4T6MmRkQUwG3Cnsc6qTyOfCFkQ46tPvXv0gm5D90V1'");
+		system("cd num1_10_file; xdg-open five.png");
 		break;
 
 	case 6:
-		system("gnome-terminal -x sh -c 'firefox https://blognumbers.files.wordpress.com/2010/09/6.jpg");
+		//system("gnome-terminal -x sh -c 'firefox https://blognumbers.files.wordpress.com/2010/09/6.jpg");
+		system("cd num1_10_file; xdg-open six.jpg");
 		break;
 
 	case 7:
-		system("gnome-terminal -x sh -c 'firefox https://images-na.ssl-images-amazon.com/images/I/31muN9eG3yL._SX355_.jpg'");
+		//system("gnome-terminal -x sh -c 'firefox https://images-na.ssl-images-amazon.com/images/I/31muN9eG3yL._SX355_.jpg'");
+		system("cd num1_10_file; xdg-open seven.jpg");
 		break;
 
 	case 8:
-		system("gnome-terminal -x sh -c 'firefox https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Huit.svg/1024px-Huit.svg.png'");
+		//system("gnome-terminal -x sh -c 'firefox https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Huit.svg/1024px-Huit.svg.png'");
+		system("cd num1_10_file; xdg-open eight.jpg");
 		break;
 
 	case 9:
-		system("gnome-terminal -x sh -c 'firefox https://s12085.pcdn.co/wp-content/uploads/2013/10/B-9.jpg'");
-		cout << " 9 " << endl;
+		//system("gnome-terminal -x sh -c 'firefox https://s12085.pcdn.co/wp-content/uploads/2013/10/B-9.jpg'");
+		system("cd num1_10_file; xdg-open nine.png");
 		break;
 
 	default:
-		system("gnome-terminal -x sh -c 'firefox https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwAocoBM-ddlESN75Yj7Ed8Sqf1f81HfR3EhrNDDoyzeZw79kU'");
+		//system("gnome-terminal -x sh -c 'firefox https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwAocoBM-ddlESN75Yj7Ed8Sqf1f81HfR3EhrNDDoyzeZw79kU'");
+		system("cd num1_10_file; xdg-open ten.jpg");
 		break;
 	}
 }
